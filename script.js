da1c90cf14171c1ede61b7e5263f249d2d992c1a
Gaurav Singh <38595651+spiritual-coder@users.noreply.github.com>
2020-11-25 18:51:14 +0530

    1.3

--- script.js
+++ script.js
@@ -1,67 +1,72 @@
 const title = document.getElementById('title');
-
 const content = document.getElementById('article-content');
-
 const btn = document.getElementById('btn');
-
 const articleList = document.getElementById('news-article');
-
 const funArticle = document.getElementById('fun-article');
-
 const dropdownChoice = document.getElementById('dropdown');
 
-
+let funart=0;
+let newsart=0;
 
 btn.onclick = function() {
 	const articleTitle = title.value;
 	const articleContent = content.value;
-
+    const articledropdown = dropdownChoice.value;
 	/*console.log(articleTitle);
 	console.log(articleContent);*/
 
 	if(articleTitle && articleContent) {
-		localStorage.setItem(articleTitle, articleContent);
-		location.reload();
+        let article={articleTitle:articleTitle,articleContent:articleContent,articledropdown:articledropdown}
+		localStorage.setItem(articleTitle, JSON.stringify(article));
+        location.reload();
+
 	}
 };
 
+
+
 	for (let i=0; i<localStorage.length; i++) {
 		const articleTitle = localStorage.key(i);
-		const articleContent = localStorage.getItem(articleTitle);
-		localStorage.clear();
+        const articleContent = localStorage.getItem(articleTitle);
+        article1=JSON.parse(articleContent)
+        console.log(article1.dropdownChoice)
 
-		if (dropdownChoice.value == 'news') {
+        if(article1.articledropdown=="news"){
+            newsart=newsart+1;
+        }
+        if(article1.articledropdown=="fun"){
+            funart=funart+1;
+        }
+
+		if (article1.articledropdown == 'news') {
+
 			articleList.innerHTML += `<div class="article-box" id="news-article">
 					<div class="article-details">
-						<h3>${articleTitle}</h3>
-						<p>${articleContent}</p>
+						<h3>${article1.articleTitle}</h3>
+						<p>${article1.articleContent}</p>
 					</div>
-
 					<div class="article-img">
 						<img src="http://lorempixel.com/300/300/" alt="new-image">
 					</div>
 				</div>
-
 				`;
 		}
 
-		else {
+		else if (article1.articledropdown == 'fun') {
 			funArticle.innerHTML += `<div class="article-box" id="news-article">
 					<div class="article-details">
-						<h3>${articleTitle}</h3>
-						<p>${articleContent}</p>
+						<h3>${article1.articleTitle}</h3>
+						<p>${article1.articleContent}</p>
 					</div>
-
 					<div class="article-img">
 						<img src="http://lorempixel.com/300/300/" alt="new-image">
 					</div>
 				</div>
-
 				`;
 		}
 
 		
-	}
+    }
+    document.getElementById('funno').innerHTML = funart;
+    document.getElementById('newsno').innerHTML = newsart;
 
-
-/*localStorage.setItem("name", "gaurav");*/
